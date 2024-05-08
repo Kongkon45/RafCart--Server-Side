1c74c4bd2d8ac619b6eebbacc87c639589a553d7
Kongkon Jowarder <kongkon4545@gmail.com>
2024-05-09 00:53:12 +0600

    vercel add

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const dev = {
     app :{
-        port : process.env.PORT || 5000
+        port : process.env.PORT || 4000
     },
     db : {
         url : process.env.DB_URL || "mongodb://localhost:27017/RafCartDb"
